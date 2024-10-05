34d23f70bd6cd9068047e5a8807e2202da87c4cd
KAM KillJoy <kamkilljoy@gmail.com>
2024-10-05 14:06:49 +0400

    закомментил дебаг принт

--- ManagerForm.py
+++ ManagerForm.py
@@ -55,7 +55,7 @@ class ManagerFormMethods(BasePage):
         ManagerFormLocators.LOCATOR_DELETE_CUSTOMER = \
             (ManagerFormLocators.LOCATOR_DELETE_CUSTOMER[0],
              ManagerFormLocators.LOCATOR_DELETE_CUSTOMER[1].replace("{row}", str(row+1)))
-        print(ManagerFormLocators.LOCATOR_DELETE_CUSTOMER)
+        #  print(ManagerFormLocators.LOCATOR_DELETE_CUSTOMER)
         self.find_element(ManagerFormLocators.LOCATOR_DELETE_CUSTOMER).click()
 '''
     def read_result_modal_title(self):
