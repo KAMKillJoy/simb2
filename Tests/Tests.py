cb01b4b89c10b89384c5107aa0c7f2f3cc77d235
KAM KillJoy <kamkilljoy@gmail.com>
2024-10-05 14:01:46 +0400

    починил обращение к переименованному модулю

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -4,7 +4,7 @@ import DeleteClient
 from confest import browser
 
 def test_new_client(browser):
-    addcustomer.new_client_add_form(browser)
+    AddCustomer.new_client_add_form(browser)
 
 def test_sort_clients(browser):
     SortClients.sort_clients(browser)
